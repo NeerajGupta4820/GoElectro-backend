fb33435ca2981b4b9d3bfbb07a12c612f0904783
Neeraj Gupta <105665359+NeerajGupta4820@users.noreply.github.com>
2024-09-30 15:42:19 +0530

    cors  package is added

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ import express from "express";
 import userRouter from "./Routes/userRoutes.js";
 import productRouter from "./Routes/productRoutes.js";
 import categoryRoutes from "./Routes/categoryRoutes.js";
+import cors from "cors"
 const app = express();
 
+app.use(cors());
+  
 
 app.use(express.json());
 
