46ab3b7c6fe8584d67dfc5ed699368d3bbf3d74d
SanjeevThalod <thalodsanjeev@gmail.com>
2024-10-28 11:46:00 +0530

    aah

--- Routes/reviewRoutes.js
+++ Routes/reviewRoutes.js
@@ -1,12 +1,18 @@
-import express from 'express';
-import {createOrUpdateReview,deleteReview,getReviewsByProductId,getReviewById,toggleLikeOrDislike} from '../controllers/reviewController.js';
-import {checkLogin} from "../Utils/jwt.js"
+import express from "express";
+import {
+  createOrUpdateReview,
+  deleteReview,
+  getReviewsByProductId,
+  getReviewById,
+  toggleLikeOrDislike,
+} from "../Controllers/reviewController.js";
+import { checkLogin } from "../Utils/jwt.js";
 const router = express.Router();
 
-router.post('/add-review',checkLogin, createOrUpdateReview);
-router.delete('/delete-review/:reviewId',checkLogin, deleteReview);
-router.get('/product/:productId', getReviewsByProductId);
-router.get('/:reviewId', getReviewById);
-router.post('/toggle-like-dislike/:reviewId', checkLogin, toggleLikeOrDislike); 
+router.post("/add-review", checkLogin, createOrUpdateReview);
+router.delete("/delete-review/:reviewId", checkLogin, deleteReview);
+router.get("/product/:productId", getReviewsByProductId);
+router.get("/:reviewId", getReviewById);
+router.post("/toggle-like-dislike/:reviewId", checkLogin, toggleLikeOrDislike);
 
 export default router;
